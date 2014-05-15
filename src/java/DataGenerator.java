d0c9984eab7c7623bada503169670c474a016c25
Rob Pagano <rob@bigcompass.com>
2014-05-15 11:32:15 -0600

    stuff...

--- src/java/DataGenerator.java
+++ src/java/DataGenerator.java
@@ -1,5 +1,3 @@
-package bc.creditmonitoring.demo
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,5 +16,49 @@ public class DataGenerator {
 
 		// spit it back out for testing purposes..
 		System.out.println("Full location is: " + fileLocation + ". Rate is " + rate + " records per minute.");
+
+		Enrollment e = new Enrollment();
+
+		System.out.println("Enrollment id is.. " + e.enrollment_id);
     }
+}
+
+class Enrollment {
+	// all fields
+	int enrollment_id; 
+	String enrollment_dtmm; 
+	String enrollment_utc;
+  	int efx_enrollee_data_src_id; 
+  	int exp_enrollee_data_src_id; 
+  	int tu_enrollee_data_src_id;
+  	String configuration_cd; 
+  	String configuration_desc; 
+  	String partner_name;
+  	String tenant_name; 
+  	String efx_status; 
+  	String efx_modify_dttm; 
+  	String efx_modify_utc;
+  	String exp_status; 
+  	String exp_modify_dtmm; 
+  	String exp_modify_utc;
+  	String tu_status; 
+  	String tu_modify_dttm; 
+  	String tu_modify_utc;
+  	String city; 
+  	String state; 
+  	String country; 
+  	int postal_cd;
+
+  	// empty constructor
+  	public Enrollment() { enrollment_id = 0; };
+	// basic constructor
+	public Enrollment(int enrollment_id, String enrollment_dtmm, String enrollment_utc,
+					  int efx_enrollee_data_src_id, int exp_enrollee_data_src_id, int tu_enrollee_data_src_id,
+					  String configuration_cd, String configuration_desc, String partner_name,
+					  String tenant_name, String efx_status, String efx_modify_dttm, String efx_modify_utc,
+					  String exp_status, String exp_modify_dtmm, String exp_modify_utc,
+					  String tu_status, String tu_modify_dttm, String tu_modify_utc,
+					  String city, String state, String country, int postal_cd) {
+		enrollment_id = enrollment_id;
+	};
 }
